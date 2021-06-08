fb280a349d79d0788e1aa20ff5216b2a35faba7f
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-08 23:32:05 +0530

    gql

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const http = require('http');
 require('dotenv').config();
 
 const app = express();
 
-app.get('/rest', (req, res) => {
+app.get('/', (req, res) => {
   res.json({
     data: 'you hit the endpoint',
   });
 });
 
+const typeDefs = gql`
+  type Query {
+    totalPosts: Int!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    totalPosts: () => 42,
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+apolloServer.applyMiddleware({ app });
+
+const httpserver = http.createServer(app);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  console.log(
+    `GraphQL Playground is on http://localhost:${process.env.PORT}/graphql`
+  );
 });
