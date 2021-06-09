b69f5b8d033c60584d7268e47770f8b050cb1e4f
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-10 00:01:24 +0530

    commit

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,9 +1,28 @@
 const { gql } = require('apollo-server-express');
+const { posts } = require('../temp');
 
-const totalPosts = () => 42;
+// queries
+const totalPosts = () => posts.length;
+const allPosts = () => posts;
+
+// mutations
+const newPost = (parent, args) => {
+  const { title, description } = args.input;
+  const post = {
+    id: posts.length + 1,
+    title,
+    description,
+  };
+  posts.push(post);
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
--- server/typeDefs/post.js
+++ server/typeDefs/post.js
@@ -1,7 +1,25 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+  }
+
   type Query {
     totalPosts: Int!
+    allPosts: [Post!]!
+  }
+
+  # input type
+  input PostInput {
+    title: String!
+    description: String!
+  }
+
+  # mutations
+  type Mutation {
+    newPost(input: PostInput!): Post!
   }
 `;
