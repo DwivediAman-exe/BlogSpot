e68d4574400ab36b3176f1db88019965c5deab23
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-11 02:30:24 +0530

    register

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
   return (
     <div className="container">
       <div className="row p-5">
         <h4>Login</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              placeholder="Enter valid Email"
+              disabled={loading}
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
+
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
   return (
-    <div className="container">
-      <div className="row p-5">
-        <h4>Register</h4>
-      </div>
+    <div className="container p-5 ">
+      <h2 className="text-center">Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div class="form-outline mb-4 p-2">
+          <input
+            id="form1Example1"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            disabled={loading}
+          />
+          <label class="form-label" for="form1Example1">
+            Email Address
+          </label>
+        </div>
+        <div class="form-outline mb-4  p-2">
+          <input type="password" id="form1Example2" class="form-control" />
+          <label class="form-label" for="form1Example2">
+            Password
+          </label>
+        </div>
+        <div class="row mb-4">
+          <div class="col">
+            <a href="#!">Forgot password?</a>
+          </div>
+        </div>
+        <button
+          className="btn btn-primary btn-raised btn-lg"
+          disabled={!email || loading}
+        >
+          Register
+        </button>
+      </form>
     </div>
   );
 };
