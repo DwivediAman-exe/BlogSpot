5fa92c2ace2fae0cf0cc586ab87eab371fc82c8d
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-13 03:26:27 +0530

    mutation

--- server/typeDefs/auth.js
+++ server/typeDefs/auth.js
@@ -4,4 +4,11 @@ module.exports = gql`
   type Query {
     me: String!
   }
+  type UserCreateResponse {
+    username: String!
+    email: String!
+  }
+  type Mutation {
+    userCreate: UserCreateResponse!
+  }
 `;
