409e1bde0f28bb35c36af2d71a025a010c5c1a1b
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-14 02:55:50 +0530

    spinner

--- client/src/components/LoadingToRedirect.js
+++ client/src/components/LoadingToRedirect.js
@@ -1,8 +1,18 @@
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
+import { css } from '@emotion/react';
+import GridLoader from 'react-spinners/GridLoader';
 
+// Can be a string as well. Need to ensure each key-value pair ends with ;
+const override = css`
+  display: block;
+  margin: 5rem auto;
+  border-color: black;
+`;
 const LoadingToRedirect = ({ path }) => {
   const [count, setCount] = useState(5);
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState('#fb9300');
   let history = useHistory();
 
   useEffect(() => {
@@ -16,9 +26,10 @@ const LoadingToRedirect = ({ path }) => {
   }, [count]);
 
   return (
-    <h3 className="container p-5 m-5  text-warning">
-      Redirecting you in {count} seconds...
-    </h3>
+    <div className="container mt-4 text-center text-warning">
+      <GridLoader color={color} loading={loading} css={override} size={30} />
+      <h3 mt-3>Redirecting you in {count} seconds...</h3>
+    </div>
   );
 };
 
