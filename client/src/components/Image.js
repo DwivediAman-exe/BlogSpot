aae0e90dd9ea59b87d19154267b7cfa73ba12bc1
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-16 16:37:36 +0530

    ui

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,10 +1,38 @@
-const Image = ({ image, handleImageRemove = (f) => f }) => {
-  return (
+const Image = ({
+  image,
+  largedisplay = false,
+  handleImageRemove = (f) => f,
+}) => {
+  return largedisplay ? (
     <img
       src={image.url}
       alt={image.public_id}
       c
-      style={{ height: '100px', margin: '2px', borderRadius: '20px' }}
+      style={{
+        height: 'auto',
+        width: 'auto',
+        maxHeight: '300px',
+        objectFit: 'cover',
+        margin: '2px',
+        borderRadius: '20px',
+      }}
+      className="img-thumbnail m-3 shadow-2-strong img-fluid"
+      onClick={() => handleImageRemove(image.public_id)}
+    />
+  ) : (
+    <img
+      src={image.url}
+      alt={image.public_id}
+      c
+      style={{
+        height: '130px',
+        width: '140px',
+        maxHeight: '130px',
+        maxWidth: '140px',
+        objectFit: 'cover',
+        margin: '2px',
+        borderRadius: '20px',
+      }}
       className="img-thumbnail m-3 shadow-2-strong img-fluid"
       onClick={() => handleImageRemove(image.public_id)}
     />
